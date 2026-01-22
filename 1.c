352ee3da13d9e604048775843d9958811b846045
Reuel Dejmal <reuel@email.cz>
2026-01-22 11:02:56 +0100

    Initial commit

---
+++ 1.c
@@ -0,0 +1,21 @@
+
+struct hrana_s;
+typedef struct hrana_s hrana_t;
+
+struct vrchol_s {
+    unsigned int id;
+    hrana_t *hrany;
+};
+typedef struct vrchol_s vrchol_t;
+
+struct hrana_s {
+    unsigned int id;
+    unsigned int hodnota;
+    vrchol_t *vrchol;
+};
+
+typedef struct {
+    vrchol_t root;
+    vrchol_t *vrcholy;
+    hrana_t *hrany;
+} graf_t;
